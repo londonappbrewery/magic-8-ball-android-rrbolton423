a7b7c18f6a54de0f3840a00d03fa7944077963ff
Romell Bolton <rrbolton@aggies.ncat.edu>
2017-07-09 23:43:45 -0500

    Add comments.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -15,26 +15,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall); // Grab the ballDisplay from the xml by the id
 
-        final int[] ballArray = {
-                R.drawable.ball1,
-                R.drawable.ball2,
-                R.drawable.ball3,
-                R.drawable.ball4,
-                R.drawable.ball5,
+        final int[] ballArray = { // The ballArray holds the 5 ball drawables
+                R.drawable.ball1, // ball1 sits at index 0
+                R.drawable.ball2, // ball2 sits at index 1
+                R.drawable.ball3, // ball3 sits at index 2
+                R.drawable.ball4, // ball4 sits at index 3
+                R.drawable.ball5, // ball5 sits at index 4
         };
 
-        Button myButton = (Button) findViewById(R.id.askButton);
+        Button myButton = (Button) findViewById(R.id.askButton); // Grab the myButton from the xml by the id
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Random randomNumberGenerator = new Random();
+            public void onClick(View v) { // Set onClickListener for myButton
+                Random randomNumberGenerator = new Random(); // Create a randomNumberGenerator object
 
-                int number = randomNumberGenerator.nextInt(4);
+                int number = randomNumberGenerator.nextInt(5); // Create a variable that stores a random number between 0 and 4
 
-                ballDisplay.setImageResource(ballArray[number]);
+                ballDisplay.setImageResource(ballArray[number]); // Set the image of the ballDisplay to a random number in the ballArray
             }
         });
 
